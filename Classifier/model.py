25e7b70a97e1308ea857ee9b850fef7905e144ee
赵旭阳 <771906409@qq.com>
2020-01-04 22:52:56 +0800

    new model

--- Classifier/model.py
+++ Classifier/model.py
@@ -6,15 +6,15 @@ class CapCNN(nn.Module):
     def __init__(self):
         super(CapCNN,self).__init__()
         self.features = nn.Sequential( #input_size=(1*100*100))
-                nn.Conv2d(1,32,kernel_size=3,stride=2,padding=1),
+                nn.Conv2d(1,64,kernel_size=3,stride=2,padding=1),
                 nn.ReLU(inplace=True),
                 nn.MaxPool2d(kernel_size=2,stride=2),
                 
-                nn.Conv2d(32,64,kernel_size=5,padding=2),
+                nn.Conv2d(64,128,kernel_size=5,padding=2),
                 nn.ReLU(inplace=True),
                 nn.MaxPool2d(kernel_size=2,stride=2),
                 
-                nn.Conv2d(64,32,kernel_size=3,padding=1),
+                nn.Conv2d(128,64,kernel_size=3,padding=1),
                 nn.ReLU(inplace=True),
                 nn.MaxPool2d(kernel_size=2,stride=2)
                 
@@ -25,9 +25,9 @@ class CapCNN(nn.Module):
         
         self.classifier = nn.Sequential(
 #                nn.Dropout(p=0.5),
-                nn.Linear(32*6*6,32),
+                nn.Linear(64*6*6,128),
                 nn.ReLU(inplace=True),
-                nn.Linear(32,4)
+                nn.Linear(128,4)
                 )
         
     def forward(self,x):
